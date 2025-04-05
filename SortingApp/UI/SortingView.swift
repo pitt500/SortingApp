6a775ef9541b677d7a1122aeee83f9974df84abe
Pedro Rojas <pedro.rojaslo50@gmail.com>
2025-04-05 13:06:04 -0700

    Create Demo data for multiple configurations

--- SortingApp/UI/SortingView.swift
+++ SortingApp/UI/SortingView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import Charts
 
 struct SortingView: View {
-    private static let initialState: [Int] = [27,8,12,54,32,42,32,54,97,14,96,9,28,35,5,41,78,11,14,96,1,18,73,91,79,65,28,80,98,99,11,19,65,78,61,31,64,41,98,10,69,99,4,62,60,11,85,26,64,25,2,77,97,52,90,76,50,72,73,46,100,16,29,52,63,5,61,71,47,89,15,36,28,83,67,46,71,10,94,77,88,71,44,71,77,13,32,54,67,73,92,42,21,35,39,22,29,58,42,15]
+    private static let initialState: [Int] = DemoData.reversed
     
     @State private var sortingType: SortingType = .bubble
     @State private var isSorting = false
