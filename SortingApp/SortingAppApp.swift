620687c37655a0fe0af85da63ee6e57ef7136cb6
Pedro Rojas <pedro.rojaslo50@gmail.com>
2025-04-08 09:41:25 -0700

    Remove unused app settings for macOS

--- SortingApp/SortingAppApp.swift
+++ SortingApp/SortingAppApp.swift
@@ -15,7 +15,29 @@ struct SortingAppApp: App {
         }
         #if os(macOS)
         .commands {
+            // Only keep essential command groups
+            CommandGroup(replacing: .appInfo) {
+                Button("About Sorting Visualizer") {
+                    // Handle about action
+                }
+            }
+            
+            // Add our custom settings commands
             MacSettingsCommands()
+            
+            // Remove View menu
+            CommandGroup(replacing: .sidebar) { }
+            
+            // Remove Window menu
+            CommandGroup(replacing: .windowList) { }
+            
+            // Remove File menu
+            CommandGroup(replacing: .newItem) { }
+            
+            
+            // Remove Edit menu
+            CommandGroup(replacing: .pasteboard) { }
+            CommandGroup(replacing: .undoRedo) { }
         }
         #endif
     }
